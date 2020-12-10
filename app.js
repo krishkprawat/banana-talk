6ff001c11e3f3541df78315c92431e247ac56b9a
krishnapal singh rawat <47602022+krishkprawat@users.noreply.github.com>
2020-12-10 19:37:07 +0530

    output variable add

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput= document.querySelector("#txt-input");
+var outputDiv= document.querySelector("#output");
 
 
-btnTranslate.addEventListener{"click", function clickEventHandler() {
-    console.log("clicked...")
-}}
+function getTranslationURL(input){
+    
+}
+
+

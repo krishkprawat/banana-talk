36a35c90f9d187a7d8dc0d203b06a0417ccd24b0
krishnapal singh rawat <47602022+krishkprawat@users.noreply.github.com>
2020-12-10 19:48:37 +0530

    add function of input output and calling server

--- app.js
+++ app.js
@@ -4,7 +4,29 @@ var outputDiv= document.querySelector("#output");
 
 
 function getTranslationURL(input){
-    
+    return serverURL +"?"+ "text=" + input
 }
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with server.")
+}
+
+functionc clickHandler() {
+    // taking input
+    var inputText=txtInput.value;
+    // calling server
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        // output
+        outputDiv.innerText = translatedText;
+
+    })
+    .catch (errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickhandler)
 
 
